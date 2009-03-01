10916535ad8ad5392062d86012158cad217e0d45
mblund <mblund@47a7a3c7-8c26-9700-2a9c-5e6216fdbc63>
2009-03-01 21:38:46 +0000

    created a test Release

--- nConfigureLib/Properties/AssemblyInfo.cs
+++ nConfigureLib/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("nConfigureLib")]
-[assembly: AssemblyDescription("released under glp 3.0")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("nconfigure@gmail.com")]
+[assembly: AssemblyCompany("https://sourceforge.net/projects/nconfigure/")]
 [assembly: AssemblyProduct("nConfigureLib")]
-[assembly: AssemblyCopyright("Copyright ©  2008,2009 Magnus Berglund")]
+[assembly: AssemblyCopyright("Copyright ©  2008,2009 Magnus Berglund, released under GPL 3.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
 
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("TestProject1")]
